228787a7b58cc671b618da7a5141f619323b446d
zo-el <joelulahanna@gmail.com>
2018-11-08 12:09:51 -0600

    update package

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,4 +1,3 @@
-import React from "react";
 import namor from "namor";
 // import "../index.css";
 
@@ -25,7 +24,7 @@ const newPerson = () => {
   };
 };
 
-export function makeData(len = 5553) {
+export default function makeData(len = 5553) {
   return range(len).map(d => {
     return {
       ...newPerson(),
