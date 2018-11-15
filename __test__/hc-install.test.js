33d613adbdfa53145630dfb411433b93379cc3cb
zo-el <joelulahanna@gmail.com>
2018-11-14 18:30:09 -0600

    added stats of all unning apps

--- __test__/hc-install.test.js
+++ __test__/hc-install.test.js
@@ -3,11 +3,21 @@ import {
   hcStart
 } from '../app/utils/hc-install'
 
+import { getStatsForPID , getPIDs } from '../app/utils/stats'
 //
 // it('Testing the Holochain install process', () => {
 //   expect(hcJoin('Errand')).toEqual("");
 // });
 
-it('Testing the Holochain start process', () => {
-  expect(hcStart('Errand', 4141)).toEqual("");
+// it('Testing the Holochain start process', () => {
+//   expect(hcStart('Errand', 4141)).toEqual("");
+// });
+
+// it('Testing the Holochain stats process', () => {
+//   expect(getStatsForPID(17740)).toEqual("");
+// });
+
+
+it('Testing the Holochain stats process', () => {
+  expect(getPIDs()).toEqual("");
 });
